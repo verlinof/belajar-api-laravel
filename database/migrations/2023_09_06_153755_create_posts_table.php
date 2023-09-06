05e14e54db802ae7e3502eef3f3a855fd36d98ef
verlinof <ajikoko.zx@gmail.com>
2023-09-06 23:00:30 +0700

    first commit

--- database/migrations/2023_09_06_153755_create_posts_table.php
+++ database/migrations/2023_09_06_153755_create_posts_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id('id');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('firstname');
-            $table->string('lastname')->nullable();
+            $table->string('title', 255);
+            $table->text('news_content');
+            $table->unsignedBigInteger('author');
             $table->timestamps();
             $table->softDeletes();
+
+            //relasi
+            $table->foreign('author')->references('id')->on('users');
         });
     }
 
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
